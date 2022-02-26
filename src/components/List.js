19df7cada030b9282fc9bc52dd0088b844ac904d
Yashwini <yashwinithakur9922@gmail.com>
2022-02-26 13:52:09 +0530

    list component changed

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,7 @@
 const List = (props) => {
   return (
     <>
+      <h1>This is list component!</h1>
       {props.users.map((user, i) => (
         <div key={i} onClick={() => props.getUserIndex(i)}>
           <h1>{user}</h1>
